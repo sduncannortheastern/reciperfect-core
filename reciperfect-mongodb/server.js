1fb45c8d22167a8d8d38ba1c46671b115bc00c1f
Steve Duncan <sduncan-msi@outlook.com>
2022-08-10 21:08:12 -0400

    allow config to be loaded even when running outside of the directory

--- reciperfect-mongodb/server.js
+++ reciperfect-mongodb/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const config = require("dotenv").config({ path: "C:\\Users\\16175\\code\\reciperfect-core\\reciperfect-mongodb\\config.env" });
+const path = require("path");
+const config = require("dotenv").config({ path: path.resolve(__dirname, "config.env") });
 const port = process.env.PORT;
+
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record.js"));
